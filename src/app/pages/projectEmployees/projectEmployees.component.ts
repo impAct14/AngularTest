84755b622d67aa3a700d50b47341a3313a446dfe
impAct <annguyenxax@gmail.com>
2025-02-04 18:08:41 +0700

    table project emp

--- src/app/pages/projectEmployees/projectEmployees.component.ts
+++ src/app/pages/projectEmployees/projectEmployees.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProjectEmployee } from '../../model/project';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import { ProjectEmployeeService } from './../../services/projectEmployee.service';
 
 @Component({
   selector: 'app-projectEmployees',
+  imports: [FormsModule, ReactiveFormsModule, DatePipe],
   templateUrl: './projectEmployees.component.html',
-  styleUrls: ['./projectEmployees.component.css']
+  styleUrls: ['./projectEmployees.component.css'],
 })
 export class ProjectEmployeesComponent implements OnInit {
+  projectEmployeeList: ProjectEmployee[] = [];
+  projectEmployee: ProjectEmployee = new ProjectEmployee();
 
-  constructor() { }
+  projectEmployeeService = inject(ProjectEmployeeService);
+
+  constructor() {}
 
   ngOnInit() {
+    this.getAllProjectEmployees();
   }
 
+  getAllProjectEmployees() {
+    this.projectEmployeeService
+      .getAllProjectEmployee()
+      .subscribe((res: ProjectEmployee[]) => {
+        this.projectEmployeeList = res;
+      });
+  }
+
+  onEdit() {}
+
+  onDelete() {}
 }
